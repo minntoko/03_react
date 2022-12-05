81330170bc9bc4e0b01fac848f831a31a0d954b9
minntoko <minntoko0822@gmail.com>
2022-12-05 22:58:53 +0900

    add-eventlistener

--- src/010_eventlistener/start/Example.js
+++ src/010_eventlistener/start/Example.js
@@ -1,6 +1,16 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('ボタンがクリックされました');
+  };
+  const clickHandler2 = () => {
+    console.log('ボタンがクリックされました。');
+  };
+  const hello = () => 'hello react';
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button onClick={clickHandler2}>クリックしてね</button>
+    </>
   );
 };
 
